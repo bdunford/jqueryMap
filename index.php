cd22b40ce14b044e79b70c7917ca46e8112b2fd5
Birch Dunford <birch.dunford@caresource.com>
2012-11-27 16:48:10 -0500

    added shape example

--- index.php
+++ index.php
@@ -15,22 +15,38 @@
 		      $('#map_canvas').gmap('openInfoWindow', {'content': 'Hello World!'}, this);
 	        });
         });
-          */  
+          */        
+                    var currLocation = '39.606772,-84.139151';
         
-    
-					$('#map_canvas').gmap({'center': '39.606772,-84.139151', 'zoom': 15, 'disableDefaultUI':true, 'callback': function() {
+   
+					$('#map_canvas').gmap({'center': currLocation, 'zoom': 15, 'disableDefaultUI':true, 'callback': function() {
 						var self = this;
-						self.addMarker({'position': this.get('map').getCenter() }).click(function() {
-							self.openInfoWindow({ 'content': 'Hello World!' }, this);
+                                               
+						self.addMarker({'id': 'current-location','position': this.get('map').getCenter() }).click(function() {
+							self.openInfoWindow({ 'content': 'Your Location' }, this);
 						});	
-                        self.addMarker({'position': '39.603772,-84.133151'});
-                        self.addMarker({'position': '39.604772,-84.133151'});
-                        self.addMarker({'position': '39.605772,-84.133151'});
-                        self.addMarker({'position': '39.602772,-84.133151'});
+                        
+                        var clientPosition = new google.maps.LatLng('39.606772','-84.139151' );
+                        
+                        self.addShape('Circle', { 
+                            'strokeWeight': 0, 
+                            'fillColor': "#008595", 
+                            'fillOpacity': 0.25, 
+                            'center': clientPosition, 
+                            'radius': 15, 
+                            'clickable': false 
+                        });
+                        
+                        self.addMarker({'position': '39.603772,-84.133151'}).click(function(){self.openInfoWindow({ 'content': 'Store 1' }, this)});
+                        self.addMarker({'position': '39.604772,-84.133151'}).click(function(){self.openInfoWindow({ 'content': 'Store 2' }, this)});
+                        self.addMarker({'position': '39.605772,-84.133151'}).click(function(){self.openInfoWindow({ 'content': 'Store 3' }, this)});
+                        self.addMarker({'position': '39.602772,-84.133151'}).click(function(){self.openInfoWindow({ 'content': 'Store 4' }, this)});
                         
 					}});
 	
-	
+
+   
+   
         
         
         
