b1269f1777ed3a8cc90cd67f17c3787e801f4142
Birch Dunford <birch.dunford@caresource.com>
2012-11-28 16:42:26 -0500

    added current location icon

--- index.php
+++ index.php
@@ -15,12 +15,13 @@
                     var currLocation = '39.606772,-84.139151';
                         
                     function Addmarkers(map) {
-                        console.log(map.get('map').getBounds());
+                        var bounds = map.get('map').getBounds();
+                        console.log(bounds);
                         map.addMarker({'position': '39.603772,-84.133151'}).click(function(){map.openInfoWindow({ 'content': 'Store 1' }, this)});
                         map.addMarker({'position': '39.604772,-84.133151'}).click(function(){map.openInfoWindow({ 'content': 'Store 2' }, this)});
                         map.addMarker({'position': '39.605772,-84.133151'}).click(function(){map.openInfoWindow({ 'content': 'Store 3' }, this)});
                         map.addMarker({'position': '39.602772,-84.133151'}).click(function(){map.openInfoWindow({ 'content': 'Store 4' }, this)});
-                        map.addMarker({'position': map.get('map').getCenter()}).click(function(){map.openInfoWindow({ 'content': 'Store 4' }, this)});
+                        //map.addMarker({'position': map.get('map').getCenter()}).click(function(){map.openInfoWindow({ 'content': 'Store 4' }, this)});
                         
                     }
         
@@ -29,20 +30,10 @@
 						
                         var map_object = this;
                                                
-						map_object.addMarker({'id': 'current-location','position': map_object.get('map').getCenter(), 'bounds':false}).click(function() {
+						map_object.addMarker({'id': 'current-location','position': map_object.get('map').getCenter(), 'bounds':false, 'icon': './images/current-location.png'}).click(function() {
 							map_object.openInfoWindow({ 'content': 'Your Location' }, this);
 						});	
                         
-                        var clientPosition = new google.maps.LatLng('39.606772','-84.139151' );
-                        
-                        map_object.addShape('Circle', { 
-                            'strokeWeight': 0, 
-                            'fillColor': "#008595", 
-                            'fillOpacity': 0.25, 
-                            'center': clientPosition, 
-                            'radius': 15, 
-                            'clickable': false 
-                        });
                           
                         google.maps.event.addListener(this.get('map'), 'tilesloaded', function() {
                             Addmarkers(map_object);
